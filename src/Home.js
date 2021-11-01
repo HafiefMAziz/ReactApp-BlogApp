e8a5bfbfb6c3584020c8233917cfec4a26d39f99
Hafief M Aziz <91103458+HafiefMAziz@users.noreply.github.com>
2021-11-01 10:40:04 +0700

    #10
    Full React Tutorial #10 - Outputting Lists

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,23 @@ import { useState } from "react";
 
 const Home = () => {
 // menggunanakan useState untuk menjadikan variable rective terhadap event
-const [name, setName] = useState('Hafief'); 
-const [age, setAge] = useState(20); 
+const [blogs, setBlogs] = useState([
 
-    const handleclick = () => {
-        setName('Maulana');
-        setAge(age + 1);
-    }
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+]); 
 
     return ( 
         <div className="Home">
             <h2>Homepage</h2>
-            <p>{ name }</p>
-            <p>{ age }</p>
-            <button onClick={handleclick}>Click Me!</button>
+            {/* Menggunakan map speprti forecah unutk menampilkan list */}
+            { blogs.map((blog)=> ( 
+                <div className="blog-preview" key={ blog.id }>
+                    <h2>{ blog.title }</h2>
+                    <p>Writen by : { blog.author }</p>
+                </div>
+            )) }
         </div>
      );
 }
